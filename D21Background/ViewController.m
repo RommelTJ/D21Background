33e71d835d3d79262408ea87e0f641f3b7d1d361
Rommel Rico <rommeltj@gmail.com>
2015-02-05 18:32:54 -0800

    Properties for ViewController in ObjC

--- D21Background/ViewController.m
+++ D21Background/ViewController.m
@@ -10,6 +10,18 @@
 
 @interface ViewController ()
 
+//These properties allow you to access the state of the switches.
+@property (weak, nonatomic) IBOutlet UISwitch *myTimerEnableSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *myBackgroundEnableSwitch;
+
+//This property allows you to access the label and update it.
+@property (weak, nonatomic) IBOutlet UILabel *myTimerCountLabel;
+
+//Property for timer count,  NSTimer, and background task id.
+@property int myTimerCount;
+@property NSTimer *myTimer;
+@property UIBackgroundTaskIdentifier myBackgroundTaskID;
+
 @end
 
 @implementation ViewController
@@ -22,6 +34,14 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//We did this because we want to dynamically respond when someone
+// changes the switch.
+- (IBAction)doTimerEnableSwitch:(id)sender {
+}
+
+- (IBAction)doBackgroundEnableSwitch:(id)sender {
 }
 
 @end
